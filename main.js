90aced24a14d4130b5e3fd99298eca5d7dc2dd98
Vlad Stepchyn <v.stepchyn@viseven.com>
2017-12-11 17:45:39 +0200

    cleaning for GitHub page

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Materialize from 'materialize-css'
-// import App from './App'
-// import router from './router'
-
-Vue.config.productionTip = false
-
-// eslint-disable-next-line to ignore the next line.
-/* eslint-disable */
 var vm = new Vue({
   el: "#example",
   data: {
